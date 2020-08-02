dedbcbab13bae652adc26b05491972fd3377fbee
Diya <diya.rajon@yahoo.com>
2020-08-02 18:37:57 -0500

    Update newEntry.js

--- newEntry.js
+++ newEntry.js
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    for(var i = 0; i < myObj.trading.length;i++)
+    for(var i = 0; i < myObj.entry.length;i++)
     {
 
     var innerDiv = document.createElement('div');
